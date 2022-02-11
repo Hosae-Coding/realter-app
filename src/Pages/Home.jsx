0ed7f48a956d39612be3568e1dabf320bf8b4bd6
LAPTOP-T21LK98P\KHS <https://github.com/Hosae-Coding>
2022-02-10 23:08:18 -0500

    second commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Sec1 from './section/Sec1';
 import Sec2 from './section/Sec2';
+import Sec3 from './section/Sec3';
 
 const Home = () => {
    return (
       <main>
          <Sec1 />
          <Sec2 />
+         <Sec3 />
       </main>
    );
 };
